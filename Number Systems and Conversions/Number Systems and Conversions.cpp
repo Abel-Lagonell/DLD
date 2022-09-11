584b8524dab3e6c7edce9e20eb3ce0e811dadd57
Abel-Lagonell <alagonell1730@floridapoly.edu>
2022-09-10 20:38:09 -0400

    Base to Base
    Something is wrong with the program
    its not outputting the right values

--- Number Systems and Conversions/Number Systems and Conversions.cpp
+++ Number Systems and Conversions/Number Systems and Conversions.cpp
@@ -55,56 +55,132 @@ int decimals(string str) {
 	return 0;
 }
 
-//int binary to Decimal Currently cannot do more than nine as base
+//int base to Decimal Currently cannot do more than nine as base
 double baseToDecimal(string str, int base) {
 
 	int len = str.length();
 	double dec_value = 0;
-	int digit = digits(str);
-	int decimal = decimals(str);
+	int l = digits(str); // *l*eftside of decimal point
+	int r = decimals(str); // *r*ightside of decimal point
 	
 	if (base < 2 || base > 9) {
 		cout << "Not valid Base" << endl;
 		return 0;
 	}
 
-	for(int i = 0; i < digit; i++){
+	for(int i = 0; i < l; i++){
 		if (str[i] == '.')
 			break;
-		dec_value += digGetter(str,digit-i) * (pow(base, digit - i - 1));
+		dec_value += digGetter(str,l-i) * (pow(base, l - i - 1));
 
 	}
 
-	if (decimal > 0) {
-		for (int i = digit + 1; i < len; i++) {
-			dec_value += digGetter(str, digit - i) / (pow(base, len - i));
+	if (r > 0) {
+		for (int i = l + 1; i < len; i++) {
+			dec_value += digGetter(str, l - i) / (pow(base, len - i));
 		}
 	}
 
 	return dec_value;
 }
 
+//double Decimal to base, currently cannot do more than nine as base
 double decimalToBase(string str, int base) {
 
 	double base_value = 0;
-	double n = stod(str), m = stod(str);
-	int digit = digits(str);
-	int decimal = decimals(str);
+	double fraction = stod(str);
+	int whole = stod(str);
+	int l = digits(str);
+	int r = decimals(str);
+	int len = str.length();
+	int i = 0;
 
-	
+	fraction -= whole;
+
+	if (base < 2 || base > 9) {
+		cout << "Not valid Base" << endl;
+		return 0;
+	}
+
+	i = 0;
+	while(whole != 0) {
+		base_value += fmod(whole,base) * pow(10,i);
+		whole = floor(whole/base);
+		i++;
+	}
+
+	i = 0;
+	if (r > 0) {
+		while (fraction != (int)fraction && i<=100) {
+			base_value += floor(base * fraction) * pow (10,-(i+1));
+			fraction = (base * fraction) - floor(base * fraction);
+			i++;
+		}
+	}
 
 	return base_value;
 }
 
-int main() {
-	string str = "33";
-	int base = 4;
+//Uses both functions to transfer from base to base
+double baseToBase(string str, int base1, int base2) {
 	
+	double dec = baseToDecimal(str, base1);
+	double base = decimalToBase(to_string(dec), base2);
 
-	double bin = baseToDecimal(str,base);
+	return base;
+}
 
-	cout << "Base " << base << " :" << str << endl;
-	cout << "Decimal: " << bin << endl;
+int main() {
+	string str;
+	char ans;
+	int base1 = 2, base2 = 3, num;
+	double bin;
+	
+	cout << "How many bases and is the original number from base 10? seperated by an ENTER" << endl;
+	cin >> num >> ans;
+
+	switch (ans) {
+	case 'y':
+	case'Y':
+		cout << "Please enter the number followed by the base seperated by an ENTER" << endl;
+		cin >> str >> base1;
+
+		bin = decimalToBase(str,base1);
+
+		cout << "Bases: " << base1 << endl;
+		break;
+	case 'n':
+	case 'N':
+		switch (num) {
+		case (2):
+			cout << "Please enter the number followed by the two bases seperated by an ENTER" << endl;
+			cin >> str >> base1 >> base2;
+
+			bin = baseToBase(str, base1, base2);
+
+			cout << "Bases: " << base1 << ", " << base2 << endl;
+			break;
+
+		case (1):
+			cout << "Please enter the number followed by the base seperated by an ENTER" << endl;
+			cin >> str >> base1;
+
+			bin = decimalToBase(str, base1);
+
+			cout << "Bases: " << base1 << endl;
+			break;
+		
+		default:
+			cout << "Cannot have more than two bases" << endl;
+		}
+		break;
+
+	default:
+		cout << "Please input Y/N" << endl;
+	}
+	
+	cout << "OG number: " << str << endl;
+	cout << "Final: " << bin << endl;
 
 	return 0;
 }
